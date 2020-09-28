5dcfa4d34ec9c28c0bf583d3f5bee75deab9e25f
Nathan Birus <nathan.birus@nih.gov>
2020-09-28 15:09:09 -0500

    uodpates

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const app = require('express')()
-const http = require('http').Server(app)
 const cors = require('cors')
+
+app.use(cors())
+app.options('*', cors())
+
+const http = require('http').Server(app)
 const PORT = process.env.PORT || 3000
 const io = require('./socket.js').init(http)
 
-// cors
-app.use(cors())
-
 require('./services/User.js')
 require('./services/Room.js')
-// require('./services/global.js')
-// const game = require('./services/game.js')
+// require('./services/game.js')
 
 // start server
 http.listen(PORT, function () {
